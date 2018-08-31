3da34510c29c1df9c68599a037f15a5bce9d3a6e
Parmesanio <sean_parmar@yahoo.com>
2018-08-31 09:54:20 -0700

    Initial front/backend setup

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import House from '../House/House';
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+
+         }
+    }
+    render() { 
+        return ( 
+            <House />
+         );
+    }
+}
+ 
+export default Dashboard;
